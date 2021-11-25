e8a3bb8017e1bcb4186e43bc3528cbaf206019cc
guiguicdd <50055027+guiguicdd@users.noreply.github.com>
2021-11-24 21:06:03 -0300

    artigos e simulador

--- assets/js/scriptArtigos.js
+++ assets/js/scriptArtigos.js
@@ -1,15 +1,49 @@
-var url = "https://www.wixapis.com/blog/v3/posts";
+var url = "https://apiwix.vercel.app/api/artigos";
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", url);
 
 xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader("Authorization", "M5Jq3MD0RYGlvtqgbnIj0eIo7jvXnYPMeur7mdCezDI.eyJpbnN0YW5jZUlkIjoiNzg3YjQ1ZTYtY2M4Mi00NDExLTljZDQtNzlmNzZhZTc5NjVhIiwiYXBwRGVmSWQiOiIyMmJlZjM0NS0zYzViLTRjMTgtYjc4Mi03NGQ0MDg1MTEyZmYiLCJtZXRhU2l0ZUlkIjoiNzg3YjQ1ZTYtY2M4Mi00NDExLTljZDQtNzlmNzZhZTc5NjVhIiwic2lnbkRhdGUiOiIyMDIxLTExLTI0VDE1OjA3OjE5LjE3MFoiLCJ1aWQiOiI4Njc0NzdkOC1mYjNjLTQ2YTEtYmZkMi1hYmYzYWI3MDc1OWMiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIiwiZGVtb01vZGUiOmZhbHNlLCJzaXRlT3duZXJJZCI6Ijg2NzQ3N2Q4LWZiM2MtNDZhMS1iZmQyLWFiZjNhYjcwNzU5YyIsInNpdGVNZW1iZXJJZCI6Ijg2NzQ3N2Q4LWZiM2MtNDZhMS1iZmQyLWFiZjNhYjcwNzU5YyIsImV4cGlyYXRpb25EYXRlIjoiMjAyMS0xMS0yNFQxOTowNzoxOS4xNzBaIiwibG9naW5BY2NvdW50SWQiOiI4Njc0NzdkOC1mYjNjLTQ2YTEtYmZkMi1hYmYzYWI3MDc1OWMifQ");
 
 xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
+    if (xhr.readyState === 4) {
+        var response = JSON.parse(xhr.responseText)
+        var showresult = document.getElementById('showResult')
+        var arrayresult = ''
+
+        for (let i = 0; i < response.data.length; i++) {
+            const artigo = response.data[i];
+
+            console.log(artigo);
+
+            arrayresult = arrayresult + `<div class="col-12 mt-4 mb-4">
+        <div class="card" onclick="openartigo('https://www.serratecnasa.com/post/${artigo.slug}')">
+            <img src="${artigo.coverMedia.image.url}" class="card-img-top"
+                alt="Logo educa sustentável">
+            <div class="card-body">
+                <h2 class="card-title">${artigo.title}</h2>
+                <p class="card-text">${artigo.excerpt}</p>
+            </div>
+        </div>
+    </div>`
+
+        }
+
+        showresult.innerHTML = arrayresult
+
+    }
+};
 
 xhr.send();
+
+
+
+
+
+
+
+const openartigo = (url) => {
+
+
+
+}
